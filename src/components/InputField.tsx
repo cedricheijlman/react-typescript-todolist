55df301ae3c9ae90dc0423db27566962754b9cd0
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-04 18:16:25 +0200

    input component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Inputfield.css";
+
+const InputField: React.FC = () => {
+  return (
+    <form className="inputForm">
+      <input />
+      <button>Go</button>
+    </form>
+  );
+};
+
+export default InputField;
